36e2ced6ef9944d593314621ded3ee22751c748a
Andrew <HoodleNoodle@outlook.com>
2021-11-08 09:24:31 +1100

    updated read methods

--- DnD_CM_WPF_Lib/Core/ReadMethods.cs
+++ DnD_CM_WPF_Lib/Core/ReadMethods.cs
@@ -8,25 +8,6 @@ namespace DnD_CM_WPF_Lib
     {
         public static string ReadString(this StreamReader reader)
             => Convert.ToString(reader.ReadToEnd());
-
-        public static string[] SplitString(this string str)
-        {
-            string[] temp = str.Split("</em>");
-            for (int i = 0; i < temp.Length; i++)
-            {
-                temp[i] = Regex.Replace(temp[i], @"<(\/|)\w*>", "");
-            }
-            return temp;
-
-        }
-        public static string Readattack(this string str)
-        {
-            str = Regex.Replace(str, @"(<(\/|)p>)|(<\/em>)", "");
-            string[] strarr = Regex.Split(str, @"<em>|</strong> ");
-            return str;
-
-        }
-
     }
 
 
